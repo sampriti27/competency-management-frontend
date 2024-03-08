55bb7645cfb4e11a80c2ef3d0282eff5188c4855
Bharani Aswath R <aswath@Bharanis-MacBook-Pro.local>
2024-03-08 18:08:41 +0530

    Latest

--- src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-//import logo from '../assets/images/logo.png';
-
-const Logo = () => {
-  // return <img src={logo} alt='jobster logo' className='logo' />;
-  return <h2 className="logo">C M S</h2>
-};
-export default Logo;
--- src/pages/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-import AddJob from './AddJob';
-import AllJobs from './AllJobs';
-import SharedLayout from './SharedLayout';
-import Stats from './Stats';
-
-export {  AddJob, Stats, SharedLayout, AllJobs };
